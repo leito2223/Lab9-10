d2a952a3f4ff8e83d51f89202a6cd8302042abfe
leito2223 <41761553+leito2223@users.noreply.github.com>
2019-06-03 21:31:46 -0600

    FrontEnd Alumnos

---
+++ app/src/main/java/com/example/lab9_10/Activity/AddUpdAlumnosActivity.java
@@ -0,0 +1,15 @@
+package com.example.lab9_10.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lab9_10.R;
+
+public class AddUpdAlumnosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_upd_alumnos);
+    }
+}
---
+++ app/src/main/java/com/example/lab9_10/Activity/AlumnosActivity.java
@@ -0,0 +1,15 @@
+package com.example.lab9_10.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lab9_10.R;
+
+public class AlumnosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumnos);
+    }
+}
