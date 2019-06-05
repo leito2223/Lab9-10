7aa6cd23b67718d06b7ce2072a5df85a5c57dae0
Renvid <minor.navarro.zuniga@est.una.ac.cr>
2019-06-04 18:44:48 -0600

    Cambios

--- app/src/main/java/com/example/lab9_10/Activity/MainActivity.java
+++ app/src/main/java/com/example/lab9_10/Activity/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.lab9_10.Activity;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,18 +18,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.lab9_10.Control.controlSQL;
+import com.example.lab9_10.Logica.Alumno;
+import com.example.lab9_10.Logica.Curso;
 import com.example.lab9_10.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    controlSQL control ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        control = new controlSQL(this);
+        try
+        {
+            Curso curso = new Curso("1","SS",3,"1");
+           // control.insertCurso(curso);
+            Log.w("Lista: ",control.listaAlumnos().toString());
+
+        }catch (Exception m)
+        {
+            Toast.makeText(this,m.getMessage(),Toast.LENGTH_SHORT);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +63,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
--- app/src/main/java/com/example/lab9_10/Control/controlSQL.java
+++ app/src/main/java/com/example/lab9_10/Control/controlSQL.java
@@ -1,54 +1,126 @@
 package com.example.lab9_10.Control;
-import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.*;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.support.annotation.Nullable;
 
-import com.example.lab9_10.R;
+import com.example.lab9_10.Logica.Alumno;
+import com.example.lab9_10.Logica.Curso;
+
+import java.util.ArrayList;
 
 
-public class controlSQL extends Activity {
-    SQLiteDatabase db;
+public class controlSQL extends SQLiteOpenHelper {
+
+    String sqlAlumno= "CREATE TABLE IF NOT EXISTS Alumno (idAlumno TEXT PRIMARY KEY, nombre TEXT,apellido TEXT,edad INTEGER)";
+    String sqlCurso= "CREATE TABLE  Curso (id TEXT PRIMARY KEY, descripcion TEXT,creditos INTEGER,idAlumno TEXT)";
+
+    public controlSQL(Context context) {
+        super(context, "labBD.db" , null, 1);
+    }
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-// filePath is a complete destination of the form
-// "/data/data/<namespace>/<databaseName>"
-// "/sdcard/<databasename>"
-        try {
-            db = SQLiteDatabase.openDatabase(
-                    "/data/data/cis493.sqldatabases/labDB",
-                    null,
-                    SQLiteDatabase.CREATE_IF_NECESSARY);
-            db.close();
-        }
-        catch (SQLiteException e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlAlumno);
+        db.execSQL(sqlCurso);
+    }
 
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS Alumno");
+        db.execSQL("DROP TABLE IF EXISTS Curso");
+        onCreate(db);
+    }
+    public boolean insertAlumno (Alumno alumno) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("idAlumno", alumno.getIdAlumno());
+        contentValues.put("nombre", alumno.getNombre());
+        contentValues.put("apellido", alumno.getApellido());
+        contentValues.put("edad", alumno.getEdad());
+        db.insert("Alumno", null, contentValues);
+        return true;
+    }
+    public boolean insertCurso (Curso curso) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id", curso.getId());
+        contentValues.put("descripcion", curso.getDescripcion());
+        contentValues.put("creditos", curso.getCreditos());
+        contentValues.put("idAlumno", curso.getIdAlumno());
+        db.insert("Curso", null, contentValues);
+        return true;
+    }
+    public boolean updateCurso (Curso curso) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id", curso.getId());
+        contentValues.put("descripcion", curso.getDescripcion());
+        contentValues.put("creditos", curso.getCreditos());
+        contentValues.put("idAlumno", curso.getIdAlumno());
+        db.update("Curso", contentValues, "id = ? ", new String[] { curso.getId() } );
+        return true;
+    }
+    public boolean updateAlumno (Alumno alumno) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("idAlumno", alumno.getIdAlumno());
+        contentValues.put("nombre", alumno.getNombre());
+        contentValues.put("apellido", alumno.getApellido());
+        contentValues.put("edad", alumno.getEdad());
+        db.update("Alumno", contentValues, "idAlumno = ? ", new String[] { alumno.getIdAlumno() } );
+        return true;
+    }
+    public Integer deleteAlumno (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("Alumno",
+                "idAlumno = ? ",
+                new String[] { id });
+    }
+    public Integer deleteCurso (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("Curso",
+                "id = ? ",
+                new String[] { id });
+    }
+    public ArrayList<Alumno> listaAlumnos() {
+        ArrayList<Alumno> array_list = new ArrayList<Alumno>();
 
-        db.beginTransaction();
-        try {
-            db.execSQL("create table tblAlumno (" +
-                    "idAlumno text PRIMARY KEY," +
-                    "nombre text," +
-                    "apellido text,"+
-                    "edad integer);");
-            db.execSQL("create table Curso (" +
-                    "id text PRIMARY KEY," +
-                    "descripcion text," +
-                    "creditos integer,"+
-                    "idAlumno text NOT NULL);");
-            db.setTransactionSuccessful(); //commit your changes
-        }
-        catch (SQLiteException e) {
-//report problem
-        }
-        finally {
-            db.endTransaction();
-        }
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from Alumno", null );
+        res.moveToFirst();
 
-    }// onCreate
+        while(res.isAfterLast() == false){
+            Alumno alumno = new Alumno();
+            alumno.setIdAlumno(String.valueOf(res.getString(res.getColumnIndex("idAlumno"))));
+            alumno.setNombre(String.valueOf(res.getString(res.getColumnIndex("nombre"))));
+            alumno.setApellido(String.valueOf(res.getString(res.getColumnIndex("apellido"))));
+            alumno.setEdad(res.getInt(res.getColumnIndex("edad")));
+            array_list.add(alumno);
+            res.moveToNext();
+        }
+        return array_list;
+    }
+    public ArrayList<Curso> listaCursos() {
+        ArrayList<Curso> array_list = new ArrayList<Curso>();
 
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from Curso", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            Curso curso = new Curso();
+            curso.setId(String.valueOf(res.getString(res.getColumnIndex("id"))));
+            curso.setDescripcion(String.valueOf(res.getString(res.getColumnIndex("descripcion"))));
+            curso.setCreditos(res.getInt(res.getColumnIndex("creditos")));
+            curso.setIdAlumno(String.valueOf(res.getString(res.getColumnIndex("idAlumno"))));
+            array_list.add(curso);
+            res.moveToNext();
+        }
+        return array_list;
+    }
 }
